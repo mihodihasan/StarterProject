d208ba235a6726dfdfb96402080eda4a45054e1d
mihodihasan <mihodihasan@gmail.com>
2018-05-10 10:59:30 +0600

    incomplete 24

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 
 
 Route::group(['middleware'=>['web']],function (){
+    Route::get('/blog/{slug}',['as'=>'blog.single','uses'=>'BlogController@getSingle']);
     Route::get('/', 'PagesController@getIndex');
     Route::get('about', 'PagesController@getAbout');
     Route::get('contact', 'PagesController@getContact');
